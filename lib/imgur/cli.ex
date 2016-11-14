e656478111268cb9001fe64a49cbd52231e9db11
sunkay <sunkay11@gmail.com>
2016-11-13 21:24:40 -0500

    CLI

--- lib/imgur/cli.ex
+++ lib/imgur/cli.ex
@@ -6,13 +6,33 @@ defmodule Imgur.CLI do
   def run(argv) do
     argv
     |> parse_args
+    |> process
   end
 
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switchs: [help: :boolean],
                                      aliases: [h: :help])
 
-    IO.inspect parse
+    case parse do
+      {_, [count], _} ->
+        {String.to_integer(count)}
+      {[], [], []} ->
+        {@default_topics}
+      {[h: true], _, _} ->
+        :help
+      _ ->
+        :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+    Usage: imgur count [--help| -h]
+    """
+  end
+
+  def process({count}) do
+    IO.puts count
   end
 
 end
